8d6cf985938eb4e0a711b5680df810bcc17ffaef
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2022-07-31 17:11:45 -0700

    Time and Space complexities addition

--- Problem1.java
+++ Problem1.java
@@ -1,3 +1,9 @@
+// Time Complexity : O(logN)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+// Your code here along with comments explaining your approach
 
 class Solution {
 	public int search(int[] nums) {
