faedbaf5256e65d6ac5803408322ea47781abf8e
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2022-07-31 17:10:24 -0700

    Missing Number in a sorted array

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,21 @@
 
+class Solution {
+	public int search(int[] nums) {
+		if (nums == null || nums.length == 0) return -1;
+		
+		if (nums[0] != 1) return 1;
+		
+		if (nums[nums.length-1] != nums.length + 1) return nums.length + 1;
+		
+		int low = 0, high = nums.length-1;
+		
+		while (low <= high) {
+			int mid = low + (high-low)/2;
+			
+			if (nums[mid] != mid + 1) high = mid - 1;
+			else low = mid + 1;
+		}
+		
+		return nums[low] - 1;
+	}
+}
